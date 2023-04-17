a2761d8cf2953dbe4d49897c33858310aecc2452
Yadvir Kaur <yadvirkaur15@gmail.com>
2023-04-17 23:01:41 +0530

    404 on Page refresh issue solved using HashRouter

--- src/index.js
+++ src/index.js
@@ -8,17 +8,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
+     <HashRouter>
       <App />
-      </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
-//When deploying a React application to a subdirectory (https://yadvirkaur.github.io/MovieList/), 
+//While using BrowserRouter: When deploying a React application to a subdirectory (https://yadvirkaur.github.io/MovieList/), 
 //you need to set the basename prop on the Router component to the subdirectory path.
 //In this case, the basename should be set to /MovieList/
+
+
+//When you deploy a React app with client-side routing (using React Router) to a static hosting service 
+//like GitHub Pages, there is a possibility that refreshing the page on a route other than the root route (/) will result in a 404 error. 
+//We can HashRouter instead of BrowserRouter as the HashRouter uses the URL hash to keep track of the current 
+//location, which allows for page refreshes without resulting in a 404 error.
+
+//Read Documentation: https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing
