aa18a5763aeeb7349cbd86e110806b63ac68605b
Yadvir Kaur <yadvirkaur15@gmail.com>
2023-04-17 18:44:20 +0530

    Working on watchlist component (#2)
    * Working on watchlist component
    
    * LocalStorage for watchlist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Row from "./components/Row";
 import MovieDetails from "./components/MovieDetails";
 import Layout from "./components/Layout";
 import Friends from "./Pages/Friends";
@@ -13,14 +10,29 @@ import Search from "./Pages/Search";
 import NotFound from "./Pages/NotFound";
 
 function App() {
+  const [watchlist, setWatchlist] = React.useState(JSON.parse(localStorage.getItem("watchlist")) || []);
+
+  React.useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  }, [watchlist]);
+
+  function toggleWatchlist(movie) {
+    if (watchlist.find(item => item.id === movie.id)) {
+      setWatchlist((prevWatchlist) =>
+      prevWatchlist.filter(item => item.id !== movie.id));
+    } else {
+      setWatchlist((prevWatchlist) => [movie, ...prevWatchlist]);
+    }
+  }
+
   return (
     <div>
      <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<MovieDetails />} />
+          <Route path="/:id" element={<MovieDetails onToggleWatchlist={toggleWatchlist} watchlist={watchlist} />} />
           <Route path="/Friends" element={<Friends />} />
-          <Route path="/Watchlist" element={<Watchlist />} />
+          <Route path="/Watchlist" element={<Watchlist  watchlist={watchlist}/>} />
           <Route path="/Search/:query" element={<Search />} />
           <Route path="/Genre/:id" element={<Genre />} />
           <Route path="*" element={<NotFound />} />
--- src/Pages/Friends.js
+++ src/Pages/Friends.js
@@ -3,7 +3,7 @@ import React from "react"
 function Friends() {
   return (
     <div>
-       <h1>Friends page goes here</h1>
+       <h3 style={{textAlign:"center"}}>Friends page goes here</h3>
     </div>
   );
 }
